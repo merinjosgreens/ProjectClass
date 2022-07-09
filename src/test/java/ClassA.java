b0ef5a4a1ff7acc635606dcf437d08f967d6294f
merinjosgreens <merinrajgreens@gmail.com>
2022-07-09 17:36:35 +0530

    Added 3 methods

--- src/test/java/ClassA.java
+++ src/test/java/ClassA.java
@@ -15,5 +15,20 @@ public class ClassA {
 		System.out.println("3");
 
 	}
+	
+	private void method4() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private void method5() {
+		// TODO Auto-generated method stub
+
+	}
+	
+	private void method6() {
+		// TODO Auto-generated method stub
+
+	}
 
 }
